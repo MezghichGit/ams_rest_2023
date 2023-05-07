f089a256114b8397d6dd44a896f5477fff289b10
moahmed amine mezghich <git@mezghich.com>
2023-05-07 15:40:39 +0100

    ams_rest_initialisation

---
+++ src/main/java/com/sip/ams/AmsApplication.java
@@ -0,0 +1,14 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsApplication.class, args);
+		System.out.println("Full Stack Project");
+	}
+
+}
